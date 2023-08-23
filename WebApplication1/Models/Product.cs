7f625c0752d993ec8fc2fe7590afc28dfe63a7d5
manav9556 <54392955+manav9556@users.noreply.github.com>
2023-08-23 22:35:06 +0530

    Product.cs

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -15,7 +15,7 @@ namespace WebApplication1.Models
         public string Description { get; set; }
         public string ImageFileName { get; set; }
         public decimal Price { get; set; }
-        public string Photo { get; set; }
+       
         public string Image { get; internal set; }
         [NotMapped]
         public IFormFile ImageFile { get; set; }
