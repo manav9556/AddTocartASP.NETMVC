3fca450a5f2087b54384352938c390e1e41cd333
manav9556 <54392955+manav9556@users.noreply.github.com>
2023-08-23 22:33:58 +0530

    CartController.cs

--- WebApplication1/Controllers/CartController.cs
+++ WebApplication1/Controllers/CartController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
-using Newtonsoft.Json;
+using WebApplication1.Repositories;
 
 namespace WebApplication1.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ProductRepository _productRepository;
+
+        public CartController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -15,40 +22,48 @@ namespace WebApplication1.Controllers
 
         public ActionResult Buy(int id)
         {
-            ProductModel productModel = new ProductModel();
             List<ProductItems> cart = HttpContext.Session.Get<List<ProductItems>>("cart") ?? new List<ProductItems>();
-            string ids=id.ToString();
-            int index = isExist(cart, ids);
-            if (index != -1)
+
+            Product product = _productRepository.GetProductById(id);
+
+            if (product != null)
             {
-                cart[index].Quantity++;
-            }
-            else
-            {
-                cart.Add(new ProductItems { Product = productModel.Find(id), Quantity = 1 });
+                int index = isExist(cart, id);
+
+                if (index != -1)
+                {
+                    cart[index].Quantity++;
+                }
+                else
+                {
+                    cart.Add(new ProductItems { Product = product, Quantity = 1 });
+                }
+
+                HttpContext.Session.Set("cart", cart);
             }
 
-            HttpContext.Session.Set("cart", cart);
             return RedirectToAction("Index");
         }
 
-        public ActionResult Remove(string id)
+        public ActionResult Remove(int id)
         {
             List<ProductItems> cart = HttpContext.Session.Get<List<ProductItems>>("cart");
             int index = isExist(cart, id);
+
             if (index != -1)
             {
                 cart.RemoveAt(index);
                 HttpContext.Session.Set("cart", cart);
             }
+
             return RedirectToAction("Index");
         }
 
-        private int isExist(List<ProductItems> cart, string id)
+        private int isExist(List<ProductItems> cart, int id)
         {
             for (int i = 0; i < cart.Count; i++)
             {
-                if (cart[i].Product.Id.Equals(id))
+                if (cart[i].Product.Id == id)
                 {
                     return i;
                 }
